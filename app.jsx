9883b0be9331590c9ba294e4f6bc59715b020479
Christos Zenonos <Christos Zenonos>
2016-04-10 23:38:07 +0100

    Initial commit

---
+++ app.jsx
@@ -0,0 +1,11 @@
+/**
+ * Created by Christos on 10/04/2016.
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HelloWorld from './hello-world';
+
+ReactDOM.render(
+<HelloWorld phrase="ES6"/>,
+    document.getElementById('content')
+);
